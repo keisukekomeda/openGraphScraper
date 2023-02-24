32c498326a29f168608acfe3ef27e075dd9c8ef0
Keisuke Komeda <keisuke.komeda@gmail.com>
2023-02-24 22:04:54 +0900

    adding ts declaration file for esm import

---
+++ dist/lib/fallback.d.ts
@@ -0,0 +1,11 @@
+/**
+ * ogs fallbacks
+ *
+ * @param {object} ogObject - the current ogObject
+ * @param {object} options - options for ogs
+ * @param {object} $ - cheerio.load() of the current html
+ * @return {object} object with ogs results with updated fallback values
+ *
+ */
+export declare function fallback(ogObject: any, options: any, $: any): any;
+export default fallback;
---
+++ dist/lib/media.d.ts
@@ -0,0 +1,10 @@
+/**
+ * formats the multiple media values
+ *
+ * @param {object} ogObject - the current ogObject
+ * @param {object} options - options for ogs
+ * @return {object} object with ogs results with updated media values
+ *
+ */
+export declare function mediaSetup(ogObject: any, options: any): any;
+export default mediaSetup;
